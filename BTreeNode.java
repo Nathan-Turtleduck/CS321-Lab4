02f1d2cccaa7d335990d5f13253a898b81064f90
Nathan Maroko <69446470+TheDankDuck@users.noreply.github.com>
2020-12-02 18:09:34 -0700

    Added BTreeNode

---
+++ BTreeNode.java
@@ -0,0 +1,18 @@
+
+public class BTreeNode {
+	
+	private int t; // Minimum degree
+	private int n; // Number of keys in subtree
+	private long[] keys; // Number of keys per Node
+	private BTreeNode[] children; // Number of children connections
+	private boolean leaf; //Boolean of whether its a leaf or not
+	
+	public BTreeNode(int t, boolean leaf) {
+		this.t = t;
+		this.leaf = leaf;
+		this.keys = new long[(2 * t)-1];
+		this.children = new BTreeNode[2 * t];
+		this.n = 0;
+	}
+	
+}
