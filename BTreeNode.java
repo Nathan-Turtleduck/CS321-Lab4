3f28051b554b02218b586232bdd756f4b6352d00
Nathan Maroko <69446470+TheDankDuck@users.noreply.github.com>
2020-12-03 13:39:50 -0700

    Formatted for TreeObjects

--- BTreeNode.java
+++ BTreeNode.java
@@ -3,14 +3,14 @@ public class BTreeNode {
 	
 	private int t; // Minimum degree
 	private int n; // Number of keys in subtree
-	private long[] keys; // Number of keys per Node
+	private TreeObject[] keys; // Number of keys per Node
 	private BTreeNode[] children; // Number of children connections
 	private boolean leaf; //Boolean of whether its a leaf or not
 	
 	public BTreeNode(int t, boolean leaf) {
 		this.t = t;
 		this.leaf = leaf;
-		this.keys = new long[(2 * t)-1];
+		this.keys = new TreeObject[(2 * t)-1];
 		this.children = new BTreeNode[2 * t];
 		this.n = 0;
 	}
