88867eff8049adfb26bb6f8109250634a44d445a
Nathan Maroko <69446470+TheDankDuck@users.noreply.github.com>
2020-12-05 13:36:20 -0700

    Added numChildren variable

--- BTreeNode.java
+++ BTreeNode.java
@@ -5,6 +5,7 @@ public class BTreeNode {
 	private int n; // Number of keys in subtree
 	private TreeObject[] keys; // Number of keys per Node
 	private BTreeNode[] children; // Number of children connections
+	private int numChildren;
 	private boolean leaf; //Boolean of whether its a leaf or not
 	
 	public BTreeNode(int t, boolean leaf) {
@@ -13,6 +14,7 @@ public class BTreeNode {
 		this.keys = new TreeObject[(2 * t)-1];
 		this.children = new BTreeNode[2 * t];
 		this.n = 0;
+		this.numChildren = 0;
 	}
 	
 	public boolean leaf() {
