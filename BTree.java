6f0bbe318e9b0a2e89918c6fabac433c69859f88
Nathan Maroko <69446470+TheDankDuck@users.noreply.github.com>
2020-12-06 03:09:20 -0700

    Added more instance variables

--- BTree.java
+++ BTree.java
@@ -1,10 +1,15 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class BTree {
 	
-	private BTreeNode root;
-	private int t;
+	private BTreeNode root; // Root of tree
+	private BTreeNode loadedNode; // Current loaded Node
+	private int t; // Degree of B-tree
+	private int currentByte; // Points to the end of file byte
+	private RandomAccessFile raf; // Random Access File to be written to
 	
-	/** Constructor for BTree (essentially it's "B-Tree-Create" **/ 
+	/** Constructor for BTree (essentially it's "B-Tree-Create") **/ 
 	public BTree(int t) {
 		this.t = t;
 //		BTreeNode x = new BTreeNode(t);
